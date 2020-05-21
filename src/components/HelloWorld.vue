162b85de634c65f54db23763a177b6b515290df7
wilphy <149201216@qq.com>
2020-05-21 21:42:04 +0800

    显示中国地图

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,56 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <!-- 有宽高的盒子,用以初始化echarts-->
+    <div ref="mapbox" style="height: 500px; width: 700px"></div>
   </div>
 </template>
 
 <script>
+import echarts from "echarts";
+import "echarts/map/js/china.js";
+
+// 使用地图前需注册
+const option = {
+  title: {
+    text: "疫情地图",
+    link: "htpps://baidu.com",
+    subtext: "加油!",
+  },
+  series: [
+    {
+      type: "map", // 要显示的类型
+      map: "china", // 显示中国地图
+      label: {
+        show: true, // 控制省名的显示
+        color: "#333",
+        fontSize: 8,
+      },
+      itemStyle: {
+        areaColor: "#eee", // 板块颜色
+        borderColor: "#aaa", // 版跨边框颜色
+      },
+      zoom: 1.2,
+      emphasis: {
+        label: {
+          show: true,
+          color: "#f0f0f0",
+          fontSize: 12,
+        },
+        itemStyle: {
+          areaColor: "#f06",
+        },
+      },
+    },
+  ]
+};
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  mounted() {
+    let mychart = echarts.init(this.$refs.mapbox);
+    mychart.setOption(option);
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
